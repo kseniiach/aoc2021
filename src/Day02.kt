5c316240e525ae3dd6ac58a7c7201dbb99b33977
Kseniia Chernikova <kseniia.chernikova@methodpark.de>
2021-12-02 15:06:06 +0100

    day 2 problem 1 solved

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,6 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var depth: Int = 0
+        var horizontal_position: Int = 0
+
+        var direction: String = ""
+        var x: Int = 0
+        var tuple: List<String>
+
+        for (line in input){
+
+            tuple = line.split(' ')
+            direction = tuple[0]
+            x = tuple[1].toInt()
+
+            when (direction){
+                "forward" -> horizontal_position += x
+                "down" -> depth += x
+                "up" -> depth -= x
+            }
+        }
+
+        return depth * horizontal_position
     }
 
     fun part2(input: List<String>): Int {
@@ -8,10 +28,10 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Input/Day02_test.txt")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Input/Day02_test")
+    check(part1(testInput) == 150)
 
-    val input = readInput("Input/Day02.txt")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("Input/Day02")
+    println("Problem 1 answer is: " + part1(input))
+    println("Problem 2 answer is: " + part2(input))
 }
