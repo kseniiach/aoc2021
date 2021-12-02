fed0a97528249df6c0778aba5c3892dea482eb98
Kseniia Chernikova <kseniia.chernikova@methodpark.de>
2021-12-02 15:52:38 +0100

    problem 2 solved

--- src/Day02.kt
+++ src/Day02.kt
@@ -23,9 +23,10 @@ fun main() {
 
         for ((direction, x) in input){
             when (direction){
-                "forward" -> horizontal_position += x
-                "down" -> depth += x
-                "up" -> depth -= x
+                "forward" -> {horizontal_position += x
+                    depth += aim * x}
+                "down" -> aim += x
+                "up" -> aim -= x
             }
         }
 
@@ -35,9 +36,9 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInputDay02("Input/Day02_test")
     check(part1(testInput) == 150)
-    //check(part2(testInput) == 900)
+    check(part2(testInput) == 900)
 
     val input = readInputDay02("Input/Day02")
     println("Problem 1 answer is: " + part1(input))
-    //println("Problem 2 answer is: " + part2(input))
+    println("Problem 2 answer is: " + part2(input))
 }
