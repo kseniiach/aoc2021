cf7f8599ddf12fbf1582c6e42f529e0ebe64dfcb
Kseniia Chernikova <kseniia.chernikova@methodpark.de>
2021-12-06 16:20:10 +0100

    Day 6 part 1

--- src/Day06.kt
+++ src/Day06.kt
@@ -4,9 +4,20 @@ fun readCommaSeparatedInputAsInt(name: String): List<Int> = File("src", "$name.t
 
 fun main() {
 
+    // really slow !
+    fun part1(input: List<Int>): Int {
+        var output = input
+        var counter: Int
 
-    fun part1(input: List<String>): Int {
-        return input.size
+        for (i in 0 until 80) {
+            counter = output.count { it == 0 }
+            output = output.map { it -> if (it == 0) 6 else it - 1}
+            for (j in 0 until counter)
+                output += 8
+            //println(output[0])
+        }
+
+        return output.size
     }
 
     fun part2(input: List<String>): Int {
@@ -15,10 +26,9 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput =  readCommaSeparatedInputAsInt("Input/Day06_test")
+    val input =  readCommaSeparatedInputAsInt("Input/Day06")
     println(testInput)
+    println(part1(testInput))
+    //println(part1(input))
     //check(part1(testInput) == 1)
-
-    val input = readInput("Input/Day02")
-    println(part1(input))
-    println(part2(input))
 }
