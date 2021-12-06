1ec83a601d805c86c86b891dad69261c0876557e
Kseniia Chernikova <kseniia.chernikova@methodpark.de>
2021-12-06 15:36:31 +0100

    reading input

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,4 +1,10 @@
+import java.io.File
+
+fun readCommaSeparatedInputAsInt(name: String): List<Int> = File("src", "$name.txt").readText().split(',').map { it.toInt() }
+
 fun main() {
+
+
     fun part1(input: List<String>): Int {
         return input.size
     }
@@ -8,10 +14,11 @@ fun main() {
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput =  readCommaSeparatedInputAsInt("Input/Day06_test")
+    println(testInput)
+    //check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = readInput("Input/Day02")
     println(part1(input))
     println(part2(input))
 }
